02520bf2c324764cfec9aa26f55ed6a0f3722334
Juan Andres <juanandres@ironhidegames.com>
2018-09-27 11:02:25 -0300

    Add quotation to username and password

--- itms_utils.rb
+++ itms_utils.rb
@@ -5,7 +5,7 @@ TRANSPORTER_PATH = '/Applications/Xcode.app/Contents/Applications/Application\ L
 
 class ITMSUtils
   def self.download_metadata(username, password, vendor_id, destination, log_name)
-    cmd = "#{TRANSPORTER_PATH} -m lookupMetadata -u #{username} -p #{password} -vendor_id #{vendor_id} -destination #{destination} &> #{log_name}"
+    cmd = "#{TRANSPORTER_PATH} -m lookupMetadata -u \"#{username}\" -p \"#{password}\" -vendor_id #{vendor_id} -destination #{destination} &> #{log_name}"
 
     unless system(cmd)
       return false
@@ -77,7 +77,7 @@ class ITMSUtils
   end
 
   def self.verify_metadata(username, password, filepath, log_name)
-    cmd = "#{TRANSPORTER_PATH} -m verify -f #{filepath} -u #{username} -p #{password} &> #{log_name}"
+    cmd = "#{TRANSPORTER_PATH} -m verify -f #{filepath} -u \"#{username}\" -p \"#{password}\" &> #{log_name}"
 
     unless system(cmd)
       return false
@@ -87,7 +87,7 @@ class ITMSUtils
   end
 
   def self.upload_metadata(username, password, filepath, log_name)
-    cmd = "#{TRANSPORTER_PATH} -m upload -f #{filepath} -u #{username} -p #{password} &> #{log_name}"
+    cmd = "#{TRANSPORTER_PATH} -m upload -f #{filepath} -u \"#{username}\" -p \"#{password}\" &> #{log_name}"
 
     unless system(cmd)
       return false
